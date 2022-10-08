82d2151e400e81599988536ff92fe84bff36deb6
Sergey Malyunov <FatalRR@yandex.ru>
2022-10-08 21:47:34 +0700

    добавил новый класс

--- src/Revise.java
+++ src/Revise.java
@@ -1,16 +1,14 @@
 public class Revise {
-    String[] month = {"Январь", "Февраль", "Март"};
-    public void reviseReports(MonthlyReport monthlyReport, YearlyReport yearlyReport) {
+    public void reviseReports(MonthlyReport monthlyReport, YearlyReport yearlyReport, Report report) {
+        if (report.checkMonth && report.checkYear) {
+            for (int i = 0; i < report.month.length; i++) {
 
-        if (monthlyReport.check && yearlyReport.check) {
-            for (int i = 0; i < month.length; i++) {
-
-                if (monthlyReport.sumExpense(i + 1) != yearlyReport.sumExpense(i + 1)) {
-                    System.out.println("Данные по расходам за " + month[i] + " не верны!!!");
+                if (monthlyReport.sumExpense(i + 1, report) != yearlyReport.sumExpense(i + 1, report)) {
+                    System.out.println("Данные по расходам за " + report.month[i] + " не верны!!!");
                 }
 
-                if (monthlyReport.sumIncome(i + 1) != yearlyReport.sumIncome(i + 1)) {
-                    System.out.println("Данные по доходам за " + month[i] + " не верны!!!");
+                if (monthlyReport.sumIncome(i + 1, report) != yearlyReport.sumIncome(i + 1,report)) {
+                    System.out.println("Данные по доходам за " + report.month[i] + " не верны!!!");
                 }
             }
             System.out.println("Операция успешно завершена.");
