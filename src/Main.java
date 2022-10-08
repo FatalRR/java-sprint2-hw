e9e677c50b345e10837873a50abd0aa44f2fb7eb
Sergey Malyunov <FatalRR@yandex.ru>
2022-10-08 21:46:44 +0700

    убрал loop, try catch, изменил вызовы

--- src/Main.java
+++ src/Main.java
@@ -1,49 +1,51 @@
 import java.util.Scanner;
-
 public class Main {
 
+//    enum command {
+//        EXIT,
+//        READ_MONTH,
+//        READ_YEAR,
+//        REVISE_REPORT,
+//        INF_MONTH_REPORT,
+//        INF_YEAR_REPORT
+//    }
+
     public static void main(String[] args) {
-        boolean startLoop = true;
         Scanner scanner = new Scanner(System.in);
+        Report report = new Report();
         MonthlyReport monthlyReport = new MonthlyReport();
         YearlyReport yearlyReport = new YearlyReport();
         Revise revise = new Revise();
         printMenu();
 
-        loop:
-
-        while (startLoop) {
-            try {
-                String userInput = scanner.nextLine();
-                int input = Integer.parseInt(userInput);
-                switch (input) {
-                    case 1:
-                        monthlyReport.readMonthReports();
-                        monthlyReport.checkRead();
-                        break;
-                    case 2:
-                        yearlyReport.readYearReport();
-                        yearlyReport.checkRead();
-                        break;
-                    case 3:
-                        revise.reviseReports(monthlyReport, yearlyReport);
-                        break;
-                    case 4:
-                        monthlyReport.printMonthlyReport();
-                        break;
-                    case 5:
-                        yearlyReport.printYearReport();
-                        break;
-                    case 453:
-                        System.out.println("Программа завершена");
-                        startLoop = false;
-                        break loop;
-                    default:
-                        System.out.println("Извините, такой команды пока нет.");
-                        break;
-                }
-            } catch (NumberFormatException s) {
-                System.out.println("Извините, такой команды пока нет.");
+        startLoop:
+        while (true) {    // 3 дня бился над Enum, не смог разобраться, не очень понятно как это использовать вместе со свитчем, не понятно как вызвать
+            // испробывал values(), ordinal(), 10 статей про него, так и не дошло, если поможете разобраться как им пользоваться именно со switch и вводом с клавиатуры, будет шикарно
+            int userInput = scanner.nextInt();
+            switch (userInput) {
+                case 1:
+                    report.readMonthReports();
+                    report.checkReadMonth();
+                    break;
+                case 2:
+                    report.readYearReport();
+                    report.checkReadYear();
+                    break;
+                case 3:
+                    revise.reviseReports(monthlyReport, yearlyReport, report);
+                    break;
+                case 4:
+                    monthlyReport.printMonthlyReport(report);
+                    break;
+                case 5:
+                    yearlyReport.printYearReport(report);
+                    break;
+                case 453:
+                    System.out.println("Программа завершена");
+                    break startLoop;
+                default:
+                    System.out.println("Извините, такой команды пока нет.");
+                    break;
             }
             printMenu();
         }
