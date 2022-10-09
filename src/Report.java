ca810d3bbda752561ded27f4847cf3f5155e469c
Sergey Malyunov <FatalRR@yandex.ru>
2022-10-09 23:36:43 +0700

    убрал лишние методы, привел к одному, передал isExpense в сигнатуру, заменил \n на сепоратор, заменил i на month

--- src/Report.java
+++ src/Report.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+
 public class Report {
     ReadFiles readFiles = new ReadFiles();
     ArrayList<MonthlyRecord> monthReports = new ArrayList<>();
@@ -8,32 +9,38 @@ public class Report {
     static final int MONTH_COUNT = 3;
     boolean checkMonth = false;
     boolean checkYear = false;
+
     public void readMonthReports() {
-        for (int i = 1; i <= MONTH_COUNT; i++) {
-            String reportLine = readFiles.readFileContentsOrNull("resources/m.20210" + i + ".csv");
-            if (reportLine == null) {
-                break;
-            }
-            System.out.println("Файл за " + this.month[i - 1] + " считан!");
+        if (monthReports.isEmpty()) {
+            for (int month = 1; month <= MONTH_COUNT; month++) {
+                String reportLine = readFiles.readFileContentsOrNull("resources/m.20210" + month + ".csv");
+                if (reportLine == null) {
+                    return;
+                }
+                System.out.println("Файл за " + this.month[month - 1] + " считан!");
 
-            String[] lines = reportLine.split("\r?\n"); // при использовании System.lineSeparator() перевод строки не осуществляется, данные не считываются, потому что файлы были созданы на mfc/linux, у меня win, поменял на регулярное выражение для кроссплатформености
-            for (int j = 1; j < lines.length; j++) {
-                String[] lineContents = lines[j].split(",");
-                String itemName = lineContents[0];
-                boolean isExpense = Boolean.parseBoolean(lineContents[1]);
-                int quantity = Integer.parseInt(lineContents[2]);
-                int sumOfOne = Integer.parseInt(lineContents[3]);
-                monthReports.add(new MonthlyRecord(itemName, isExpense, quantity, sumOfOne, i));
+                String[] lines = reportLine.split("\r?\n");
+                for (int j = 1; j < lines.length; j++) {
+                    String[] lineContents = lines[j].split(",");
+                    String itemName = lineContents[0];
+                    boolean isExpense = Boolean.parseBoolean(lineContents[1]);
+                    int quantity = Integer.parseInt(lineContents[2]);
+                    int sumOfOne = Integer.parseInt(lineContents[3]);
+                    monthReports.add(new MonthlyRecord(itemName, isExpense, quantity, sumOfOne, month));
+                }
             }
-        }
-        checkMonth = true;
-    }
-    public void readYearReport() {
-        String yearFile = readFiles.readFileContentsOrNull("resources/y.2021.csv");
-        if (yearFile == null) {
-            checkYear = false;
+            checkMonth = true;
         } else {
+            System.out.println("Отчет уже загружен!");
+        }
+    }
 
+    public void readYearReport() {
+        if (yearReport.isEmpty()) {
+            String yearFile = readFiles.readFileContentsOrNull("resources/y.2021.csv");
+            if (yearFile == null) {
+                return;
+            }
             String[] lines = yearFile.split("\r?\n");
             for (int i = 1; i < lines.length; i++) {
                 String[] lineContents = lines[i].split(",");
@@ -42,18 +49,22 @@ public class Report {
                 boolean isExpense = Boolean.parseBoolean(lineContents[2]);
                 yearReport.add(new YearlyRecord(month, amount, isExpense));
             }
-            checkYear = true;
+            System.out.println("Файл за " + year[0] + " считан!");
+
+        } else {
+            System.out.println("Отчет уже загружен!");
         }
+        checkYear = true;
     }
+
     public void checkReadMonth() {
         if (!checkMonth) {
             System.out.println("Файлы не считаны!");
         }
     }
+
     public void checkReadYear() {
-        if (checkYear) {
-            System.out.println("Файл за " + year[0] + " год считан!");
-        } else {
+        if (!checkYear) {
             System.out.println("Файл не считан!");
         }
     }
