848afc8c68201a606deaff53d88e9f89758e62ea
Sergey Malyunov <FatalRR@yandex.ru>
2022-10-09 23:37:46 +0700

    убрал лишние методы, привел к одному, передал isExpense в сигнатуру, заменил \n на сепоратор, заменил i на month

--- src/YearlyReport.java
+++ src/YearlyReport.java
@@ -1,55 +1,36 @@
 public class YearlyReport {
-    public int sumExpense(int num, Report report) {
+    public int sumExpenseOrIncome(int num, boolean isExpense, Report report) {
         int sum = 0;
         for (YearlyRecord row : report.yearReport) {
-            if (row.isExpense && row.month == num) {
+            if (row.isExpense == isExpense && row.month == num) {
                 sum += row.amount;
             }
         }
         return sum;
     }
 
-    public int sumIncome(int num, Report report) {
-        int sum = 0;
-        for (YearlyRecord row : report.yearReport) {
-            if (!row.isExpense && row.month == num) {
-                sum += row.amount;
-            }
-        }
-        return sum;
-    }
-
-    public String avgExpense(Report report) {
+    public String avgExpenseOrIncome(boolean isExpense, Report report) {
         int sum = 0;
         int temp = 0;
         for (YearlyRecord row : report.yearReport) {
-            if (row.isExpense) {
+            if (row.isExpense == isExpense) {
                 sum += row.amount;
                 temp++;
             }
         }
-        return "Средний расход за год: " + sum / temp;
+        return " за год: " + sum / temp;
     }
 
-    public String avgIncome(Report report) {
-        int sum = 0;
-        int temp = 0;
-        for (YearlyRecord row : report.yearReport) {
-            if (!row.isExpense) {
-                sum += row.amount;
-                temp++;
-            }
-        }
-        return "Средний доход за год: " + sum / temp;
-    }
     public void printYearReport(Report report) {
         if (report.checkYear) {
             System.out.println("Информация за " + report.year[0] + " год:");
             for (int i = 0; i < report.MONTH_COUNT; i++) {
-                System.out.println("Прибыль за " + report.month[i] + ": " + (sumIncome(i + 1, report) - sumExpense(i + 1, report)));
+                System.out.println("Прибыль за " + report.month[i] + ": " +
+                        (sumExpenseOrIncome(i + 1, false, report) -
+                                sumExpenseOrIncome(i + 1, true, report)));
             }
-            System.out.println(avgExpense(report) + "\n" +
-                    avgIncome(report));
+            System.out.println("Средний расход " + avgExpenseOrIncome(true, report) + "\n" +
+                    "Средний доход " + avgExpenseOrIncome(false, report));
         } else {
             System.out.println("Файл не считан!");
         }
